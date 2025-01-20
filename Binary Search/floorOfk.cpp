7c982257db6bad704df175aec1b5e106928abaf4
chandradiproy <official.chandradip@gmail.com>
2025-01-20 23:23:02 +0530

    floor of k

--- Binary Search/floorOfk.cpp
+++ Binary Search/floorOfk.cpp
@@ -3,38 +3,30 @@
 
 using namespace std;
 
-int findFloor(vector<int> &arr, int k)
+int findFloor(vector<int> &nums, int target)
 {
-
-    int l = 0, r = arr.size() - 1, mid = 0;
-    while (l <= r)
+    int mid = 0, left = 0, right = nums.size() - 1, ans = -1;
+    while (left <= right)
     {
-        mid = l + (r - l) / 2;
-        if (arr[mid] <= k)
+        mid = left + (right - left) / 2;
+        if (nums[mid] >= target)
         {
-            return mid;
+            ans=mid;
+            right = mid - 1;
         }
-        else
-        {
-            if (arr[mid] > k)
-            {
-                r = mid - 1;
-            }
-            else
-            {
-                l = mid + 1;
-            }
+        else{
+            left = mid + 1;
         }
     }
-    return -1;
+    return ans;
 }
 
 //{ Driver Code Starts.
 int main()
 {
     vector<int> v1 = {1, 2, 8, 10, 12, 19};
-    cout << findFloor(v1, 5) << endl;  // 1
-    cout << findFloor(v1, 20) << endl; // 5
+    cout << findFloor(v1, 2) << endl;  // 1
+    cout << findFloor(v1, 5) << endl; // 5
     vector<int> v2 = {1, 3, 5, 7, 10};
     cout << findFloor(v2, 4) << endl; // 2
     return 0;
